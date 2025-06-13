cd731f7467d459005075c5cb9f7c1b6a6d913f71
Surender Soundiramourty <psm.surender@gmail.com>
2025-06-12 23:10:20 -0400

    chore: add config header for game parameters

---
+++ common/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+constexpr int CLIENT_TIMEOUT_MS = 5000; // 5 seconds
+constexpr int SERVER_PORT = 9000; // Default server port
+
+// Game configuration constants
+constexpr int MIN_PLAYERS = 2; // Minimum players to start the game
+constexpr int MAX_PLAYERS = 4; // Maximum players allowed in the game
+constexpr int WAIT_TIME_SEC = 15; // Time to wait for players before starting the game
