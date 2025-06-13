196132aabd8c1c71527094ca06aa31c4c90170f2
Surender Soundiramourty <psm.surender@gmail.com>
2025-06-13 05:40:29 -0400

    fix(server): threaded update loop improves packet loss due to more consistent ticks.

--- server/server.cpp
+++ server/server.cpp
@@ -10,10 +10,10 @@
 #include "game_manager.h"
 #include "../common/config.h"
 #include "utils.h"
+#include <thread>
 
 #define PORT 9000
 #define BUFFER_SIZE 1024
-#define BROADCAST_INTERVAL_MS 100
 
 int main() {
     int sockfd;
@@ -38,13 +38,19 @@ int main() {
 
     std::cout << "[START] UDP server running on port " << PORT << std::endl;
     GameManager game_manager(MAX_PLAYERS, WAIT_TIME_SEC);
-    auto last_broadcast_time = std::chrono::steady_clock::now();
+
+    std::thread([&game_manager, &sockfd]() {
+        while (true) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(BROADCAST_INTERVAL_MS));
+            game_manager.update();
+            game_manager.broadcastToAll(sockfd);
+        }
+    }).detach();
 
     while (true) {
         socklen_t len = sizeof(client_addr);
         ssize_t n = recvfrom(sockfd, buffer, BUFFER_SIZE - 1, MSG_DONTWAIT,
                              (sockaddr*)&client_addr, &len);
-        game_manager.update();
         if (n > 0) {
             buffer[n] = '\0';
             std::string msg(buffer);
@@ -52,17 +58,10 @@ int main() {
             auto response = game_manager.handleMessage(msg, client_addr);
             if (response.has_value()) {
                 sendto(sockfd, response->c_str(), response->size(), 0,
-                    (const sockaddr*)&client_addr, sizeof(client_addr));
+                       (const sockaddr*)&client_addr, sizeof(client_addr));
             }
         }
-
-        auto now = std::chrono::steady_clock::now();
-        if (std::chrono::duration_cast<std::chrono::milliseconds>(now - last_broadcast_time).count() >= BROADCAST_INTERVAL_MS) {
-
-            game_manager.broadcastToAll(sockfd);
-
-            last_broadcast_time = now;
-        }
+    
     }
 
     close(sockfd);
